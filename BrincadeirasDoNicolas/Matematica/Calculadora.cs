c63c3143a6482ecae02f236dbc9aaaf9cb728083
nicolas.martins <nicolas.martins@PCIBRA61>
2022-06-13 16:40:08 -0300

    Adicionar arquivos de projeto.

---
+++ BrincadeirasDoNicolas/Matematica/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿
+namespace BrincadeirasDoNicolas.Matematica
+{
+    public class Calculadora
+    {
+        double valorTotal = 0;
+        string Operador = "";
+        public double obterValor()
+        {
+            return valorTotal;
+        }
+        public void calculo(double num)
+        {
+            if (Operador == "+")
+            {
+                valorTotal = valorTotal + num;
+            }
+            else if (Operador == "-")
+            {
+                valorTotal = valorTotal - num;
+            }
+            else if (Operador == "*")
+            {
+                valorTotal = valorTotal * num;
+            }
+            else if (Operador == "/")
+            {
+                valorTotal = valorTotal / num;
+            }
+            else
+            {
+                valorTotal = num;
+            }
+        }
+
+        public string OP(string operador)
+        {
+            Operador = operador;
+            return Operador;
+
+        }
+
+        internal string obterOperador()
+        {
+            return Operador;
+        }
+    }
+}
---
+++ BrincadeirasDoNicolas/Program.cs
@@ -0,0 +1,51 @@
+﻿using BrincadeirasDoNicolas.Matematica;
+
+namespace BrincadeirasDoNicolas
+{
+    public class Program
+    {
+        private static string[] OPERADORES = new string[] { "+", "-", "*", "/" };
+
+        public static void Main(string[] args)
+        {
+            string receba = "";
+
+            Console.WriteLine("Bem vindo ao programa do nicolas. Informe apenas um número para iniciar");
+
+            Calculadora calculadora = new Calculadora();
+            while (true)
+            {
+                while (true)
+                {
+                    receba = Console.ReadLine();
+                    if (double.TryParse(receba, out double num))
+                    {
+                        calculadora.calculo(num);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.Write($"={calculadora.obterValor()}{calculadora.obterOperador()}");
+                    }
+                }
+
+                Console.Clear();
+                Console.Write($"={calculadora.obterValor()}");
+
+                while (true)
+                {
+                    var operador = Console.ReadKey().KeyChar.ToString();
+                    if (OPERADORES.Contains(operador))
+                    {
+                        calculadora.OP(operador);
+                        break;
+                    }
+
+                }
+                Console.Clear();
+                Console.Write($"={calculadora.obterValor()}{calculadora.obterOperador()}");
+            }
+        }
+    }
+}
